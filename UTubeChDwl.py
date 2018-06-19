50d442f017d3c74f05a475c837fc7817677603f7
vmalep <vmalep@gmail.com>
2018-06-19 15:38:27 -0500

    Add files via upload

--- UTubeChDwl.py
+++ UTubeChDwl.py
@@ -10,36 +10,42 @@ import urllib, re
 
 #print(urllib.request.urlopen(channel_url).getcode()) AttributeError: 'module' object has no attribute 'request'
 
-mylines = ""
+# The regex substring to search for in the source page:
+substr = '\?v\=[A-Za-z0-9]+'
 
-while True:
-    try:
-        channel_url = raw_input("Please enter the YoutTube channel url here ('exit' to stop): ")
-        if channel_url == "exit": break
-        if channel_url[-6:len(channel_url)] != "videos":
-            channel_url += "/videos"
-        if channel_url[0:4] != "http":
-            channel_url = "https://" + channel_url
-        print "reading " + channel_url
-        mylines = urllib.urlopen(channel_url).readlines()
-        print "done"
-        break
-    except:
-        print "Oops!  That was not a valid url.  Try again..."
+mylines = ""
+count = 0
+video_id = []
+all_lines = ""
+
+# Asking for the url
+#channel_url = raw_input("Please enter the YoutTube channel url here ('exit' to stop): ")
+channel_url = "https://www.youtube.com/channel/UCXbWsGV_cjG3gOsSnNJPVlg/videos"
+# Checking if the url start with https and ends with "videos"
+#if channel_url[-6:len(channel_url)] != "videos":
+#    channel_url += "/videos"
+#if channel_url[0:4] != "http":
+#    channel_url = "https://" + channel_url
+
+print "reading " + channel_url
+
+# test if the url works
+#try:
+mylines = urllib.urlopen(channel_url).readlines()
+print "done: " + str(len(mylines)) + " lines copied"
+#except:
+#    print "Oops!  That was not a valid url.  Try again..."
 
 #print mylines
 
-count = 0
+# Concatenate Read each line and if it matches the substring, add it to the video_id dict
 for item in mylines:
-    if "VideoId" in item:
-        count += 1
-        video_id = re.findall("\VideoId\b\"\:\"[A-Za-z0-9]+", item)
-#    print("Original string: ",text)
-        if video_id: print video_id
+    all_lines += item
+#print all_lines
 
-print count
-    #if 'gridVideoRenderer' in item:
-    #    count += 1
-    #    print count
-    #    print item
-    #    #print item[item.index("videoId"):]
+video_id = re.findall(substr, all_lines)
+print video_id
+
+#for i in video_id[]
+#    print i
+
